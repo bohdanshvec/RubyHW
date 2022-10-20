96a42a7279f4589c03f23e21d85e2dadbcce6b72
bohdanshvetc <dan6147@ukr.net>
2022-10-20 16:10:32 +0300

    Решено 22 задачи

--- HW1/1.rb
+++ HW1/1.rb
@@ -1,3 +1,4 @@
 p arr = [87, 23, 28, 56, 73, 20, 51, 29, 15, 58, 51, 30, 25, 71, 82, 23, 59, 11, 15, 36]
 p "Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными."
-p arr.select.with_index { |x, i| x if i.even? } + arr.select.with_index { |x, i| x if i.odd? }
+p arr.partition.with_index { |x, i| i.even? }.flatten
+
