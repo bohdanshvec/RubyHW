5bcb62f43450490281a82fb00a1f4f5bb973a6af
bohdanshvetc <dan6147@ukr.net>
2022-11-20 21:28:28 +0200

    update articles_controkker

--- HW5/stories/app/controllers/api/v1/articles_controller.rb
+++ HW5/stories/app/controllers/api/v1/articles_controller.rb
@@ -1,34 +1,38 @@
 class Api::V1::ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show update destroy ]
+
   def index
     @articles = Article.all
-
     render json: @articles
   end
 
   def create
-    @article = Article.new(title: params[:title], body: params[:body])
+    @article = Article.new(params_article)#(title: params[:title], body: params[:body])
     @article.save
-
     render json: @article
   end
 
   def show
-    @article = Article.find(params[:id])
-
     render json: @article
   end
 
   def update
-    @article = Article.find(params[:id])
-    @article.update(body: params[:body])
-
+    @article.update(params_article)#(body: params[:body])
     render json: @article
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.delete
+      render json: :ok
+  end
 
-    render json: :ok
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def params_article
+    params.require(:article).permit(:title, :body)
   end
 end
