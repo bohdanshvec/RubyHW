1d9606519e9c6fd917e23067422c5ef211a8c062
bohdanshvetc <dan6147@ukr.net>
2022-12-31 22:15:12 +0200

    HW11 task 1-6

--- HW11/Shop/app/controllers/line_items_controller.rb
+++ HW11/Shop/app/controllers/line_items_controller.rb
@@ -1,9 +1,14 @@
 class LineItemsController < ApplicationController
   def create
-    byebug
     product = Product.find(params[:product_id])
-    current_cart.add_product(product)
-
+    if LineItem.where(product_id: params[:product_id]).present?
+      new_quantity = LineItem.where(product_id: params[:product_id]).first.quantity + 1
+      LineItem.find_by(product_id: params[:product_id]).update(quantity: new_quantity)
+      new_price = LineItem.where(product_id: params[:product_id]).first.price + product.price
+      LineItem.find_by(product_id: params[:product_id]).update(price: new_price)
+    else
+      current_cart.add_product(product)
+    end
     redirect_to cart_path, notis: "#{product.name} was successfully added to the cart."
   end
 end
--- HW11/Shop/app/models/cart.rb
+++ HW11/Shop/app/models/cart.rb
@@ -2,6 +2,12 @@ class Cart < ApplicationRecord
   has_many :line_items, dependent: :nullify
 
   def add_product(product)
-    line_items.create(product: product, quantity: 1)
+    line_items.create(product: product, quantity: 1, price: product.price)
+    # byebug
+  end
+
+  def summ_price
+    line_items.map(&:price).sum
+    # byebug
   end
 end
